3c16f351b592d6a400b166bdec62dafd46b6967b
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-02-15 22:09:06 +0000

    MVP 12 tests passing

--- models/dinosaur.js
+++ models/dinosaur.js
@@ -5,34 +5,51 @@
 }
 
 
-Dinosaur.prototype.find_dinosaur_that_attracts_most_visitors = function(dinosaurs){
-  let dinosaur_that_attracts_most_guests = dinosaurs[0].guestsAttractedPerDay
-  for (var i = 1; i < dinosaurs.length; i++) {
-    if (dinosaurs[i].guestsAttractedPerDay > dinosaur_that_attracts_most_guests) {
-      dinosaur_that_attracts_most_guests = dinosaurs[i].guestsAttractedPerDay
+// Dinosaur.prototype.find_dinosaur_that_attracts_most_visitors = function(dinosaurs){
+//   let array_of_number_of_visitors = [];
+// }
+
+
+Dinosaur.prototype.find_dinosaur_by_species = function(dinosaurs, species){
+  let dinosaurs_of_particular_species = []
+  for(var dino of dinosaurs) {
+    if (dino.species === species) {
+      dinosaurs_of_particular_species.push(dino)
     }
   }
-  return dinosaur_that_attracts_most_guests
+  return dinosaurs_of_particular_species;
 }
 
 
-// Dinosaur.prototype.find_dinosaur_by_species = function(dinosaurs, species){
-//   let dinosaurs_of_particular_species = []
-//   for(var dino in dinosaurs) {
-//     if (dino.species === species) {
-//       dinosaurs_of_particular_species.push(dino)
-//     }
-//   }
-//   return dinosaurs_of_particular_species;
-// }
+Dinosaur.prototype.calculate_visitors_per_day = function(dinosaurs){
+  let total = 0;
 
+  for(var dino of dinosaurs){
+    total += dino.guestsAttractedPerDay
+  }
 
-// Park.prototype.calculate_visitors_per_day = function(dinosaurs){
+  return total;
+}
 
-// }
+Dinosaur.prototype.calculate_total_number_of_visitors_per_year = function(dinosaurs) {
+  let total = 0
 
-// Park.prototype.calculate_visitors_per_year = function() {}
+  for(var dino of dinosaurs) {
+    total += dino.guestsAttractedPerDay
+  }
 
-// Park.prototype.calculate_total_revenue_per_year = function() {}
+  return total * 365
+}
+
+Dinosaur.prototype.calculate_total_revenue_per_year = function(dinosaurs) {
+  let total = 0
+
+  for(var dino of dinosaurs) {
+    total += dino.guestsAttractedPerDay
+  }
+
+  return total * 365 * 50
+}
+
 
 module.exports = Dinosaur;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -7,13 +7,15 @@ describe('Park', function() {
   let park;
   let dinosaur1;
   let dinosaur2;
-  let dinosaurs;
+  let dinosaur3;
+  let dinosaur4;
 
   beforeEach(function () {
-    park = new Park('Jurassic', 50, ['t-rex', 'triceratops', 'diplodok']);
-    dinosaur1 = new Dinosaur('Velociraptor', 'carnivore', 50)
-    dinosaur2 = new Dinosaur('Allosaurus', 'carnivore', 70)
-    dinosaurs = [dinosaur1, dinosaur2]
+    dinosaur1 = new Dinosaur('T-Rex', 'carnivore', 15000)
+    dinosaur2 = new Dinosaur('Allosaurus', 'carnivore', 7000)
+    dinosaur3 = new Dinosaur('Stegosaurus', 'herbivorous', 12000)
+    dinosaur4 = new Dinosaur('Triceratops', 'herbivorous', 14000)
+    park = new Park('Jurassic', 50, [dinosaur1, dinosaur2, dinosaur3]);
   })
 
   it('should have a name', function() {
@@ -28,33 +30,45 @@ describe('Park', function() {
 
   it('should have a collection of dinosaurs', function() {
     const actual = park.collection_of_dinosaurs;
-    assert.deepStrictEqual(actual, ['t-rex', 'triceratops', 'diplodok'])
+    assert.deepStrictEqual(actual, [dinosaur1, dinosaur2, dinosaur3])
   });
 
   it('should be able to add a dinosaur to its collection', function() {
-    park.add_dinosaur_to_collection('allosaurus')
+    park.add_dinosaur_to_collection(dinosaur4)
     const actual = park.collection_of_dinosaurs
-    assert.deepStrictEqual(actual, ['t-rex', 'triceratops', 'diplodok', 'allosaurus'])
+    assert.deepStrictEqual(actual, [dinosaur1, dinosaur2, dinosaur3, dinosaur4])
   });
 
   it('should be able to remove a dinosaur from its collection', function() {
     park.remove_dinosaur_from_collection()
     const actual = park.collection_of_dinosaurs;
-    assert.deepStrictEqual(actual, ['t-rex', 'triceratops'] )
+    assert.deepStrictEqual(actual, [dinosaur1, dinosaur2] )
   });
 
-  it('should be able to find the dinosaur that attracts the most visitors', function() {
-    const actual = dinosaur1.find_dinosaur_that_attracts_most_visitors(dinosaurs)
-    assert.deepStrictEqual(actual, 70)
+  xit('should be able to find the dinosaur that attracts the most visitors', function() {
+    const actual = dinosaur1.find_dinosaur_that_attracts_most_visitors(park.collection_of_dinosaurs)
+    assert.deepStrictEqual(actual, dinosaur1)
+  });
 
+  it('should be able to find dinosaurs by species', function() {
+    const actual = dinosaur1.find_dinosaur_by_species(park.collection_of_dinosaurs, 'Stegosaurus')
+    assert.deepStrictEqual(actual, [dinosaur3])
 
   });
 
+  it('should be able to calculate the total number of visitors per day', function() {
+    const actual = dinosaur1.calculate_visitors_per_day(park.collection_of_dinosaurs)
+    assert.strictEqual(actual, 34000)
+  });
 
-  xit('should be able to calculate the total number of visitors per day');
+  it('should be able to calculate the total number of visitors per year', function() {
+    const actual = dinosaur1.calculate_total_number_of_visitors_per_year(park.collection_of_dinosaurs)
+    assert.strictEqual(actual, 12410000)
+  });
 
-  xit('should be able to calculate the total number of visitors per year');
-
-  xit('should be able to calculate total revenue for one year');
+  it('should be able to calculate total revenue for one year', function() {
+    const actual = dinosaur1.calculate_total_revenue_per_year(park.collection_of_dinosaurs);
+    assert.strictEqual(actual, 620500000)
+  });
 
 });
